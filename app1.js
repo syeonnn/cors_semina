77a38cb2e5b83908e3f3d5ddae866a72d2b543ca
Seoyeon Park <qkrtj418@gmail.com>
2024-07-30 00:16:33 +0900

    feat: simple request 예제 구현

--- app1.js
+++ app1.js
@@ -1,22 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('form');
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("form");
 
-    form.onsubmit = (e) => {
-        e.preventDefault();
+  form.onsubmit = (e) => {
+    e.preventDefault();
 
-        const xhr = new XMLHttpRequest();
-        const formData = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData(form);
 
-        xhr.onload = () => {
-            if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-                const responseData = xhr.responseText; // responseText: 서버로부터 받은 응답 데이터
-                const result = JSON.parse(responseData); // JSON 역직렬화
-                console.log(result); 
-            }
-        }
+    // 로그로 formData의 내용을 출력
+    for (let [key, value] of formData.entries()) {
+      console.log(key, value);
+    }
 
-        xhr.open('POST', '/simple');
-
-        xhr.send(formData);
+    xhr.onload = () => {
+      if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+        const responseData = xhr.responseText; // responseText: 서버로부터 받은 응답 데이터
+        const result = JSON.parse(responseData); // JSON 역직렬화
+        alert("축하드립니다. 맛있는 하루였네요 ^___^");
+      }
     };
+
+    xhr.open("POST", "/simple");
+    // xhr.setRequestHeader("Content-type", "multipart/form-data");
+    xhr.send(formData);
+  };
 });
