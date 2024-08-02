3cea89dd28c24137e03e385eaaf9392a573254b3
Seoyeon Park <qkrtj418@gmail.com>
2024-08-02 11:55:09 +0900

    feat: preflight 예제 구현

--- app1.js
+++ app1.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("form");
+  const btn = document.getElementById("preflightButton");
+  const responseContainer = document.getElementById("reponse-result");
 
   form.onsubmit = (e) => {
     e.preventDefault();
@@ -7,21 +9,41 @@ document.addEventListener("DOMContentLoaded", () => {
     const xhr = new XMLHttpRequest();
     const formData = new FormData(form);
 
-    // 로그로 formData의 내용을 출력
-    for (let [key, value] of formData.entries()) {
-      console.log(key, value);
-    }
-
     xhr.onload = () => {
       if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-        const responseData = xhr.responseText; // responseText: 서버로부터 받은 응답 데이터
-        const result = JSON.parse(responseData); // JSON 역직렬화
+        const responseData = xhr.responseText;
+        const result = JSON.parse(responseData);
         alert("축하드립니다. 맛있는 하루였네요 ^___^");
       }
     };
 
-    xhr.open("POST", "/simple");
-    // xhr.setRequestHeader("Content-type", "multipart/form-data");
+    xhr.open("POST", "http://localhost:3000/simple");
     xhr.send(formData);
   };
+
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "http://localhost:3000/data");
+    xhr.send();
+
+    xhr.onload = () => {
+      if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+        const responseData = xhr.responseText;
+        const result = JSON.parse(responseData);
+        console.log(result);
+        console.log(result.name, result.favoriteFood);
+        responseContainer.textContent = `${result.name} 의 최애음식 ${result.favoriteFood}`;
+      } else {
+        console.log(`error: ${xhr.status}`);
+      }
+    };
+
+    // fetch("http://localhost:3000/data", { method: "GET" })
+    //   .then((res) => res.json())
+    //   .then((data) => console.log(data))
+    //   .catch((err) => console.error("Error:", err));
+  });
 });
