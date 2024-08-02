3da8146876e1bc2fbf77f84163e81f7b66ff58e5
Seoyeon Park <qkrtj418@gmail.com>
2024-08-02 12:35:53 +0900

    feat: content-type 추가

--- app1.js
+++ app1.js
@@ -24,26 +24,45 @@ document.addEventListener("DOMContentLoaded", () => {
   btn.addEventListener("click", (e) => {
     e.preventDefault();
 
-    const xhr = new XMLHttpRequest();
+    // const xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "http://localhost:3000/data");
-    xhr.send();
+    // xhr.open("GET", "http://localhost:3000/data");
+    // xhr.send();
 
-    xhr.onload = () => {
-      if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-        const responseData = xhr.responseText;
-        const result = JSON.parse(responseData);
+    // xhr.onload = () => {
+    //   if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+    //     const responseData = xhr.responseText;
+    //     const result = JSON.parse(responseData);
+    //     console.log(result);
+    //     console.log(result.name, result.favoriteFood);
+    //     responseContainer.textContent = `${result.name} 의 최애음식 ${result.favoriteFood}`;
+    //   } else {
+    //     console.log(`error: ${xhr.status}`);
+    //   }
+    // };
+
+    fetch("http://localhost:3000/data", {
+      method: "GET",
+      headers: {
+        "X-Custom-Header": "CustomValue", // 커스텀 헤더 추가
+        "Content-Type": "application/json", // Content-Type을 application/json으로 설정
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((result) => {
         console.log(result);
         console.log(result.name, result.favoriteFood);
-        responseContainer.textContent = `${result.name} 의 최애음식 ${result.favoriteFood}`;
-      } else {
-        console.log(`error: ${xhr.status}`);
-      }
-    };
 
-    // fetch("http://localhost:3000/data", { method: "GET" })
-    //   .then((res) => res.json())
-    //   .then((data) => console.log(data))
-    //   .catch((err) => console.error("Error:", err));
+        // 응답 결과를 표시
+        responseContainer.textContent = `${result.name} 의 최애음식 ${result.favoriteFood}`;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   });
 });
